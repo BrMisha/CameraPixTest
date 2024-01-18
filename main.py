248720ccfaa63778ca58d853443185ab17cb7108
Michael Brazhnikov <br.misha@gmail.com>
2024-01-18 16:31:19 +0200

    done

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
-import asyncio
 import time
 import cv2
-from pymavlink.dialects.v20 import common as mavlink2
 from pymavlink import mavutil
 import threading
+import queue
+import sqlite3
 
 pitch = None
-lock = asyncio.Lock()
+lock = threading.Lock()
+
+queue = queue.Queue()
+
 
 def thread_mavlink():
     master = mavutil.mavlink_connection("/dev/ttyACM0", 921600)
     master.wait_heartbeat()
     while True:
         msg = master.recv_match(type='ATTITUDE', blocking=True)
-        '''lock.acquire()
-        pitch = msg.pitch
-        lock.release()'''
-        print(time.monotonic_ns(), msg.pitch, msg.roll, msg.yaw)
+        lock.acquire(True)
+        global pitch
+        pitch = (time.monotonic(), msg.pitch)
+        lock.release()
 
 
 def thread_camera():
@@ -26,21 +29,43 @@ def thread_camera():
         if result is None:
             break
 
-        lock.acquire().
+        lock.acquire(True)
+        global pitch
         p = pitch
+        # print(time.monotonic_ns(), p)
         lock.release()
-        print(time.monotonic_ns(), p)
+        queue.put((p[0], p[1], time.monotonic(), image))
+
+
+def thread_db():
+    db = sqlite3.connect("db.sqlite")
+    cursor = db.cursor()
+    cursor.execute("""CREATE TABLE "main" (
+        "index"	INTEGER NOT NULL UNIQUE,
+        "time_p"	INTEGER NOT NULL,
+        "pitch"	REAL,
+        "time_i"	INTEGER,
+        "img"	BLOB,
+        PRIMARY KEY("index" AUTOINCREMENT)
+    );""")
+    db.commit()
+    while True:
+        item = queue.get()
+        print(item[0])
+        im = cv2.imencode('.jpg', item[3])[1]
+        cursor.execute(""" INSERT INTO main (time_p, pitch, time_i, img) VALUES (?, ?, ?, ?)""",
+                       (item[0], item[1], item[2], im))
+        db.commit()
 
 
 t1 = threading.Thread(target=thread_mavlink)
 t2 = threading.Thread(target=thread_camera)
+t3 = threading.Thread(target=thread_db)
 
 t1.start()
 t2.start()
+t3.start()
 
 t1.join()
 t2.join()
-
-'''
-
-'''
+t3.join()
